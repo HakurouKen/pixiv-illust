c93becfeb5c96812b826de1118dece89cdf67da5
hakurouken <ticshot@gmail.com>
2016-10-03 09:15:28 +0800

    Basic login

--- src/login.js
+++ src/login.js
@@ -3,34 +3,72 @@ import cheerio from 'cheerio';
 import 'babel-polyfill';
 
 const request = Promise.promisifyAll(require('request'));
+const fs = Promise.promisifyAll(require('fs'));
 
 const HOST = 'www.pixiv.net';
-const LOGIN_PAGE = 'https://accounts.pixiv.net/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index';
-const LOGIN_API = 'https://accounts.pixiv.net/api/login?lang=zh'
+const ACCOUNT_HOST = 'accounts.pixiv.net';
+const LOGIN_PAGE = `https://${ACCOUNT_HOST}/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index`;
+const LOGIN_API = `https://${ACCOUNT_HOST}/api/login?lang=zh`;
+const LOGOUT_URL = `http://${HOST}/logout.php?return_to=%2F`;
 
 class Login {
     constructor(){
         this.cookieJar = request.jar();
+        this.pending = null;
     }
 
-    _getPostKey(){
-        return request.getAsync({
+    get cookies(){
+        return this.cookieJar.getCookies(`http://${HOST}`);
+    }
+
+    get loggedIn(){
+        const keys = ['PHPSESSID','device_token'];
+        let cookieKeys = this.cookies.map(cookie => cookie.key);
+        return keys.every(key => cookieKeys.includes(key));
+    }
+
+    async _getPostKey(){
+        let resp = await request.getAsync({
             url: LOGIN_PAGE,
             jar: this.cookieJar
-        }).then(resp => {
-            if (resp.statusCode !== 200) return Promise.reject(resp);
-            let $ = cheerio.load(resp.body);
-            let jsonData = $('#init-config').attr('value');
-            let data = JSON.parse(jsonData);
-            return data['pixivAccount.postKey'];
         });
+
+        if (resp.statusCode !== 200) return Promise.reject(resp);
+        let $ = cheerio.load(resp.body);
+        let jsonData = $('#init-config').attr('value');
+        let data = JSON.parse(jsonData);
+        return data['pixivAccount.postKey'];
+    }
+
+    _setPendingPromise(){
+        let _resolve,_reject;
+        let self = this;
+        self.pending = new Promise((resolve,reject) => {
+            _resolve = (...args) =>{
+                self.pending = null;
+                resolve.call(self,args);
+            };
+            _reject = (...args) => {
+                self.pending = null;
+                reject.call(self,args);
+            };
+        });
+        return [_resolve,_reject];
     }
 
     async login(account,password){
+        let [_resolve,_reject] = this._setPendingPromise();
+
         let postKey = await this._getPostKey();
-        return request.postAsync({
+        let resp = await request.postAsync({
             url: LOGIN_API,
             jar: this.cookieJar,
+            headers: {
+                'Host': ACCOUNT_HOST,
+                'Origin': `https://${ACCOUNT_HOST}`,
+                'Referer': LOGIN_PAGE,
+                'X-Requested-With': 'XMLHttpRequest'
+            },
             form: {
                 pixiv_id: account,
                 password: password,
@@ -39,26 +77,58 @@ class Login {
                 post_key: postKey,
                 source: 'pc'
             }
-        }).then(resp => {
-            if (resp.statusCode !== 200) {
-                return Promise.reject(resp);
-            }
-            // there are two types of data returned when sucessed:
-            // 1. nothing
-            // 2. '{"error":false,"message":"","body":{"successed":{"return_to":"http:\/\/www.pixiv.net\/"}}}'
-            // the `error` key cannot be used to judge whether the request is successed,
-            // it's always false.
-            if (!resp.body) return;
-            let ret = JSON.parse(resp.body);
-            if (ret.body && ret.body.successed){
-                return ret;
-            }
+        });
+
+        if (resp.statusCode !== 200) {
+            _reject(resp);
             return Promise.reject(resp);
+        }
+        // there are two types of data returned when sucessed:
+        // 1. nothing
+        // 2. '{"error":false,"message":"","body":{"successed":{"return_to":"http:\/\/www.pixiv.net\/"}}}'
+        // the `error` key cannot be used to judge whether the request is successed,
+        // it's always false.
+        let ret = JSON.parse(resp.body || null);
+        if (!ret || ret.body && ret.body.successed){
+            _resolve(ret);
+            return ret;
+        }
+        _reject(ret);
+        return Promise.reject(resp);
+    }
+
+    async logout() {
+        return await request.getAsync({
+            url: LOGOUT_URL,
+            jar: this.cookieJar
         });
     }
 
-    getCookies(){
-        return this.cookieJar.getCookies(`http://${HOST}`);
+    async dumps(file) {
+        return await fs.writeFileAsync(
+            file,
+            JSON.stringify(this.cookieJar._jar.toJSON(),null,4)
+        );
+    }
+
+    async loads(file) {
+        let [_resolve,_reject] = this._setPendingPromise();
+        try {
+            // A hack to load json cookiejar from file.
+            let content = await fs.readFileAsync(file);
+            const CookieJar = this.cookieJar._jar.constructor;
+            this.cookieJar._jar = CookieJar.fromJSON(content.toString());
+            _resolve(this);
+            return this;
+        } catch(err) {
+            _reject(err);
+            throw err;
+        }
+    }
+
+    reset() {
+        this.cookieJar = request.jar();
+        return this;
     }
 }
 
--- test/login.js
+++ test/login.js
@@ -1,48 +1,138 @@
 import { expect } from 'chai';
 import Promise from 'Bluebird';
+import path from 'path';
+import fs from 'fs';
 import login from '../dist/login';
-const config = require('./config');
+const account = require('./account.privacy.json');
 import 'babel-polyfill';
 
-describe('login',() => {
-    describe('#getCookies()',() => {
-        it('should return the cookies array',() => {
-            expect(login.getCookies()).to.be.an('array');
-        });
-    });
+const ASYNC_REQUEST_TIMEOUT = 10000;
 
-    describe('#_getPostKey()', () => {
-        it('should return a Promise resolved with postKey', done => {
-            let postKey = login._getPostKey();
-            expect(postKey).to.be.an.instanceof(Promise);
-            postKey.then(ret => {
-                expect(ret).to.match(/^[a-z0-9]{32}$/);
-                done();
-            })
-        });
-    });
-
-    describe('#login()',() => {
-        let user = login.login(config.USERNAME,config.PASSWORD);
-        it('should return a Promise',() => {
-            expect(user).to.be.an.instanceof(Promise);
-        });
-
-        it('should set cookie properly',done => {
-            user.then(() => {
-                let cookies = login.getCookies();
-                let keys = cookies.map(cookie => cookie.key);
-                expect(keys).to.have.members(['PHPSESSID','p_ab_id','device_token']);
-                done();
-            }).catch(err => {
-                throw err;
+describe('login', function() {
+    describe('when not logged in:', function() {
+        describe('get#cookies()', function() {
+            it('should return an empty array', function() {
+                let cookies = login.cookies;
+                expect(cookies).to.be.an('array');
+                expect(cookies.length).to.equal(0);
             });
-        }).timeout(5000);
+        });
 
-        it('should reject when login info is invalid',done => {
-             login.login('FAKE_USER_ACCOUNT','WRONG_PASSWORD').catch(() => {
-                 done();
-             });
-        }).timeout(5000);
+        describe('get#loggedIn()', function() {
+            it('should be false', function() {
+                expect(login.loggedIn).to.equal(false);
+            });
+        });
+
+        describe('#_getPostKey()', function() {
+            it('should return a Promise resolved with postKey (32 digits)', function(done) {
+                let postKey = login._getPostKey();
+                expect(postKey).to.be.an.instanceof(Promise);
+                postKey.then(function(ret){
+                    expect(ret).to.match(/^[a-z0-9]{32}$/);
+                    done();
+                });
+            }).timeout(ASYNC_REQUEST_TIMEOUT);
+        });
+    });
+
+    describe('login and logout:', function() {
+        function makeLoginTest(desc,getUser){
+            describe(desc, function() {
+                let user,pendingPromise;
+                before(function() {
+                    user = getUser();
+                    pendingPromise = login.pending;
+                });
+
+                it('should return a promise', function() {
+                    expect(user).to.be.an.instanceof(Promise);
+                });
+
+                it('should set a pending promise', function() {
+                    expect(pendingPromise).to.be.an.instanceof(Promise);
+                });
+
+                it('should clear the pending status', function(done) {
+                    user.then(function() {
+                        expect(login.pending).to.be.null;
+                        done();
+                    });
+                }).timeout(ASYNC_REQUEST_TIMEOUT);
+
+                it('should set cookies properly', function(done) {
+                    user.then(function() {
+                        let keys = login.cookies.map(cookie => cookie.key);
+                        expect(keys).to.includes.members(['PHPSESSID','device_token','p_ab_id']);
+                        done();
+                    });
+                }).timeout(ASYNC_REQUEST_TIMEOUT);
+
+                it('should resolve the pending promise when resolved', done => {
+                    pendingPromise.then(function() {
+                        let keys = login.cookies.map(cookie => cookie.key);
+                        expect(keys).to.includes.members(['PHPSESSID','device_token','p_ab_id']);
+                        done();
+                    });
+                }).timeout(ASYNC_REQUEST_TIMEOUT);
+
+                it('can be reset correctly', function() {
+                    login.reset();
+                    expect(login.cookies).to.be.empty;
+                });
+            });
+        }
+
+        makeLoginTest('#login(account,password)', function() {
+            return login.login(account.USERNAME,account.PASSWORD);
+        });
+
+        makeLoginTest('#loads(file)', function() {
+            let file = path.join(__dirname,'cookie.privacy.json');
+            return login.loads(file);
+        });
+    });
+
+    describe('when logged in:', function() {
+        let user;
+        before(() => {
+            // user = login.login(account.USERNAME,account.PASSWORD);
+            user = login.loads(path.join(__dirname,'cookie.privacy.json'));
+        });
+
+        describe('get#cookies()', function() {
+            it('should return an cookie array', function(done) {
+                user.then(function(){
+                    let cookies = login.cookies;
+                    expect(cookies).to.be.an('array');
+                    cookies.forEach(cookie => {
+                        expect(cookie).to.have.property('key');
+                        expect(cookie).to.have.property('value');
+                    });
+                    done();
+                });
+            }).timeout(ASYNC_REQUEST_TIMEOUT);
+        });
+
+        describe('get#loggedIn()', function() {
+            it('should be true', function(done) {
+                user.then(() => {
+                    expect(login.loggedIn).to.equal(true);
+                    done();
+                });
+            }).timeout(ASYNC_REQUEST_TIMEOUT);
+        });
+
+        describe('dumps(file)', function() {
+            it('should dumps to the specific file', function(done) {
+                let file = path.join(__dirname,'output-cookie.privacy.json');
+                login.dumps(file).then(() => {
+                    fs.readFile(file,function(err,data){
+                        JSON.parse(data.toString());
+                        done();
+                    });
+                });
+            }).timeout(ASYNC_REQUEST_TIMEOUT);
+        });
     });
 });
