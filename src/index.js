8421dcea1b8bc99f6853202ef157bf231f0c88c8
hakurouken <ticshot@gmail.com>
2016-10-19 00:16:57 +0800

    index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import login from './login';
+import Illust from './illust';
+import Rank from './rank';
+import Bookmark from './bookmark';
+import Author from './author';
+
+export {
+    login,
+    Illust,
+    Rank,
+    Bookmark,
+    Author
+};
