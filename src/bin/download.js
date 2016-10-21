9ed33eec45525dfc4a4888e756b25e2ef7776935
hakurouken <ticshot@gmail.com>
2016-10-21 20:00:32 +0800

    Bookmarks download cli.

--- src/bin/download.js
+++ src/bin/download.js
@@ -2,11 +2,11 @@ import 'babel-polyfill';
 import fs from 'fs';
 import path from 'path';
 import program from 'commander';
-import { login,Illust } from '..';
+import { login,Bookmark,Illust } from '..';
 
 // basic path settings.
 let BASE_PATH,cookieFile;
-export function setup(root=__dirname) {
+export function setup(root='') {
     BASE_PATH = root;
     cookieFile = path.join(root,'.cookies.json');
     return program.parse(process.argv);
@@ -34,7 +34,33 @@ program
         let illust = new Illust(id);
         let folder = program.dest || '';
         await illust.download(
-            path.join(folder,'{{author}} - {{title}}{{suffix}}')
+            path.join(BASE_PATH,folder,'{{author}} - {{title}}{{suffix}}')
         );
+        console.log(`Download successfully.`);
+    });
+
+const downloadList = async(list, name='{{author}} - {{title}}{{suffix}}') => {
+    for (let info of list) {
+        try {
+            let illust = new Illust(info.illust_id);
+            await illust.download(name);
+        } catch (err) {
+            console.error(`ID: ${info && info.id} download error.`);
+        }
+    }
+}
+
+program
+    .command('bookmark')
+    .action(async () => {
+        await loginAction();
+        let bookmark = new Bookmark();
+        let firstPage = await bookmark.get();
+        let total = firstPage.total;
+        await downloadList(firstPage.contents);
+        for (let page = 2; page < firstPage; page++) {
+            let list = await bookmark.getPageContent(page);
+            await downloadList(list);
+        }
         console.log(`Download successfully.`);
     });
